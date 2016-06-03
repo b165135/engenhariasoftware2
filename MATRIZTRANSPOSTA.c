b1e3e86b5c2ff0c0f0a3de45a0d70f233ae7c980
b165135 <bacs30@gmail.com>
2016-06-03 20:06:05 -0300

    Implementação em c [COMPLEMENTO]
    Modificações no código fonte implementado em C

--- MATRIZTRANSPOSTA.c
+++ MATRIZTRANSPOSTA.c
@@ -14,13 +14,28 @@ int main () {
 		//se houver mais que 20 colunas ou menos que 1 coluna, tamanho inválido
 		if (coluna<20 && coluna>1){
 			//declaração da matriz
-			int matriz[linha][coluna];
+			float matriz[linha][coluna];
+			int aux;
 			//pegando os elementos da matriz por linha
 			for (contadorLinha = 0 ; contadorLinha < linha ; contadorLinha++){
 				//pegando os elementos da matriz por coluna
 				for(contadorColuna = 0; contadorColuna < coluna; contadorColuna++){
 					//printf("Digite o valor da posição na coluna %d, da linha %d: ", contadorColuna+1, contadorLinha+1);
-					scanf("%d", &matriz[contadorLinha][contadorColuna]);
+					scanf("%f", &matriz[contadorLinha][contadorColuna]);
+				}
+			}
+			for (contadorLinha = 0 ; contadorLinha < linha ; contadorLinha++){
+				//pegando os elementos da matriz por coluna
+				for(contadorColuna = 0; contadorColuna < coluna; contadorColuna++){
+					aux=matriz[contadorLinha][contadorColuna];
+					if(aux==matriz[contadorLinha][contadorColuna]){
+						//OK
+					}
+					else {
+						printf("Insercao de elementos da matriz de forma incorreta\n");
+						return 0;
+					}
+					aux=0;
 				}
 			}
 			//Mostra a matriz dada
@@ -39,7 +54,7 @@ int main () {
 			for (contadorLinha = 0 ; contadorLinha < coluna ; contadorLinha++){
 				//pega os elementos da matriz por coluna e muda para linha
 				for(contadorColuna = 0; contadorColuna < linha; contadorColuna++){
-					printf("%d", matriz[contadorColuna][contadorLinha]);
+					printf("%.0f", matriz[contadorColuna][contadorLinha]);
 					if (contadorColuna!=linha-1){
 						printf(" ");
 					}
@@ -48,10 +63,10 @@ int main () {
 			}
 			//printf("\n");
 		}else{
-			printf("O tamanho %d para a coluna é inválido.\n", coluna);
+			printf("O tamanho %d para a coluna esta fora do limite permitido.\n", coluna);
 		}
 	}else{
-		printf("O tamanho %d para a linha é inválido.\n", linha);
+		printf("O tamanho %d para a linha esta fora do limite permitido.\n", linha);
 	}
 
 	return 0;
